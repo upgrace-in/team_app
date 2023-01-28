12c034868a0269e7a993645dbf927bd241f66916
upgrace-in <thedesiretree47@gmail.com>
2023-01-28 10:13:28 +0530

    work

--- src/console/Console.js
+++ src/console/Console.js
@@ -217,7 +217,7 @@ export default function Dashboard(props) {
                         <li className="mb-1">
                             <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
                                 data-bs-target="#home-collapse" aria-expanded="true">
-                                Main
+                                Admin Main
                             </button>
                             <div className="collapse show" id="home-collapse">
                                 <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
